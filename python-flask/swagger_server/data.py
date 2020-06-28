167b3adb3de3f5083a1dc9782dd563c18839ca2e
Frank Tropschuh <gunther@idoru.ch>
2020-06-28 11:28:38 +0200

    delegate to core routines

--- python-flask/swagger_server/data.py
+++ python-flask/swagger_server/data.py
@@ -5,13 +5,9 @@ CONFIG.read("/data/picky.ini")
 
 # set of channels with hosts and their services
 from swagger_server.models.channel import Channel
-DATA = {
+DATA = {}
 
-    "gold": Channel.from_dict({
-        "name": "gold",
-    }),
-    "silver": Channel.from_dict({
-        "name": "silver",
-    }),
+from swagger_server.controllers.channels_controller import alerts_channel
 
-}
+alerts_channel("gold", {"name": "gold"})
+alerts_channel("silver", {"name": "silver"})
