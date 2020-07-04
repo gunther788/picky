248353ca0184714ffa7b3ecbfc16fc2b309091d6
Frank Tropschuh <gunther@idoru.ch>
2020-07-04 08:13:37 +0200

    cleanup

--- python-flask/swagger_server/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.channel import Channel
-from swagger_server.models.host import Host
-from swagger_server.models.service import Service
